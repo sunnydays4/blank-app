8cf4f5825ad6157a05f369dbb969e73e95ce320e
sunnydays4 <day-fewest.09@icloud.com>
2025-07-24 13:59:05 +0000

    Correction des bugs et amélioration de l'affichage

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,8 @@ st.write("Outil réalisé par [Mathias Pisch](https://www.linkedin.com/in/mathia
 st.image(logo_url, width=200)
 # Upload de l'image
 uploaded_file = st.file_uploader("Glissez-déposez une image ici", type=["png", "jpg", "jpeg"])
-st.image(uploaded_file, width=200)
+if uploaded_file :
+    st.image(uploaded_file, width=200)
 
 # Choix des couleurs
 st.markdown("### Couleurs à détecter")
