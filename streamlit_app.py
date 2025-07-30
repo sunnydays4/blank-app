e71e0a3336660dc9079922184a5cfe5725441324
sunnydays4 <day-fewest.09@icloud.com>
2025-07-30 12:28:51 +0000

    Correction des bugs et amélioration de l'affichage

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ st.title("🖼️ Analyse d'images - Plan de masse")
 
 st.write("Pour savoir ce que fait cet outil, et comment l'utiliser, vous pouvez consulter la [documentation.](https://docs.google.com/presentation/d/e/2PACX-1vRxz5DE5uva9u3Uvqn1mU_ylCjGdndhxH_I_OZOBeHeFB6kRP1bo-b7rqyquY4hJ_0dxUsGc_hejEEd/pub?start=false&loop=false&delayms=3000)")
 
-st.write("Outil réalisé par [Mathias Pisch](https://www.linkedin.com/in/mathiaspisch/)")
+st.write("Outil développé par [Mathias Pisch](https://www.linkedin.com/in/mathiaspisch/)")
 st.image(logo_url, width=200)
 # Upload de l'image
 uploaded_file = st.file_uploader("Glissez-déposez une image ici", type=["png", "jpg", "jpeg"])
